8f28bfbcb558d1da1ba07b2eb58c9bf062e5a69e
DiInfantas <127537754+DiInfantas@users.noreply.github.com>
2023-11-24 10:41:53 -0300

    arreglos api

--- rest_api/views.py
+++ rest_api/views.py
@@ -10,8 +10,24 @@ from .serializers import ViajeSerializer
 # Create your views here.
 
 @csrf_exempt
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def lista_viajes(request):
-    viajes = Viaje.objects.all()
-    serializer = ViajeSerializer(viajes, many=True)
-    return Response(serializer.data)
+
+
+    if request.method == 'GET':
+        viajes = Viaje.objects.all()
+        serializer = ViajeSerializer(viajes, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = ViajeSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response (serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        Viaje.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    
